42976580fefcb1a5d6eae9440bc6891ab5c72ec9
Anna <goryachevaanny@gmail.com>
2023-07-25 18:14:43 +0300

    add some function in js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ let changesButton = document.querySelector('.left_button');
 
 changeButton.onclick = function() {
     topBlock.classList.toggle('top__right-change');
+    const myImg = document.getElementById('myImg');
+    myImg.src = '/assets/image/02.png';
 };
 
 changesButton.onclick = function() {
     topBlock.classList.toggle('top__right-change');
+    const myImg = document.getElementById('myImg');
+    myImg.src = '/assets/image/01.png';
 };
